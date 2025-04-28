4d80b68a763a48c23b8f5c19d4f97ca956557e36
polishchuk-s <84209055+polishchuk-s@users.noreply.github.com>
2025-04-29 01:32:46 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     pageLoadTimeout: 60000,
+    "fixturesFolder": "cypress/fixtures",
+    "specPattern": "**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
